bd70e989b6408be56053d05b60a66c57b22500a0
Antti Stenvall <antti@stenvall.fi>
2014-06-23 09:26:06 +0300

    Added comments to demo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /* 
- * Test file for testing json2map class 
+ * Test file for testing json2map class. This works out as a tutorial too!
  * 
  */
 
@@ -11,9 +11,11 @@ int main(int argc, char **argv) {
     std::cout << "Call this as ./main json-inputfile" << std::endl;
     return EXIT_SUCCESS;
   }  
+  // read JSON file name from command line parameter (test.json included in the repo)
   std::string inputFile = std::string(argv[1]);
   // init json2map structure
   json::json2map *json = new json::json2map(inputFile);      
+  // print all that is inside the json
   json->prettyPrint();
   std::cout << std::endl << std::endl;
   // exaple of testing keys
@@ -21,15 +23,19 @@ int main(int argc, char **argv) {
     std::cout << "Given key does not exists" << std::endl;
   }  
   if (json->keyExists("pi","data")){    
-    std::cout << "Double: " << json->getValue<double>("pi","data") << std::endl;
-  }
+    // init double from json to a variable
+    double d = json->getValue<double>("pi","data");
+    std::cout << "Double: " << d << std::endl;
+  }  
   if (json->keyExists("firstNumbers","data")){
+    // retrieve vector of values
     std::vector<int> temp = json->getVector<int>("firstNumbers","data");
     std::cout << "Array" << std::endl;
     for (unsigned int i=0;i<temp.size();i++){
       std::cout << "Value " << i << ": " << temp.at(i) << std::endl;
     }
   }    
+  // test value types
   std::vector<std::string> empty;
   std::cout << "Value type of root is: " << json->getValueType(empty) << std::endl;
   if (json->keyExists("temp")){
@@ -38,7 +44,7 @@ int main(int argc, char **argv) {
   if (json->keyExists("name")){
     std::cout << "Value type of name is: " << json->getValueType("name") << std::endl;
   }
-  // init vector of json2maps
+  // init vector of json2maps from an existing value in json2map, note the valueType comparison
   if (json->keyExists("characters") && 
           json->getValueType("characters") == json_spirit::array_type){
     std::vector<json::json2map*> jsonV = json->getVector("characters");
@@ -58,4 +64,3 @@ int main(int argc, char **argv) {
   delete json;
   return EXIT_SUCCESS;
 }
-
